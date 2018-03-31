cc70b0f538b7275aa38a6f59640daa52ffffea0d
Brad Harris <bradharris@Brad-Harris.local>
2018-03-30 17:02:16 -0700

    left off chats.js

---
+++ app/chats.js
@@ -0,0 +1,57 @@
+const rp = require("request-promise");
+const _ = require('lodash');
+
+// Docs - https://docs.livechatinc.com/rest-api/#get-list-of-chats
+function request_agent_chats(options, agent_chats_data) {
+  agent_chats_data = agent_chats_data;
+  options.qs.page = options.qs.page || 1;
+  return rp(options)
+  .auth(process.env.EMAIL, process.env.API_KEY)
+  .then((res) => {
+    // push chats to agent_chats_data with only data/columns that we need
+    res.chats.forEach((chat) => {
+      let filtered_chat = _.pick(chat, chat_model);
+      agent_chats_data.push(filtered_chat);
+    })
+
+    // collect responses from page 2 onward (livechat provides 25 chats per page)
+    if (options.qs.page < res.pages) {
+      options.qs = _.merge(options.qs, {"page":(options.qs.page + 1)});
+      setTimeout(next_request, 1500);
+      function next_request() {
+        return request_agent_chats(options);
+      }
+    } else {
+      console.log("AGENT CHATS: ", agent_chats_data);
+      // console.log("\n\n\nAgent: ", options.qs.agent);
+      // console.log("Total chats: ", agent_chats_data.length);
+      return Promise.resolve(agent_chats_data);
+    }
+  })
+  .catch((err) => {
+    return Promise.reject('Error: ', err)
+  })
+};
+
+// returns a promise with a value of all chats for a specific agent
+exports.agent_chats = function(agent) {
+  let agent_chats_data = [];
+  let chat_params = {
+    date_from: "2018-02-01",
+    date_to: "2018-02-28",
+    timezone: "America/Los_Angeles",
+    agent: agent
+  }
+  let options = {
+    uri: "https://api.livechatinc.com/v2/chats",
+    json: true,
+    qs: chat_params
+  }
+  return request_agent_chats(options, agent_chats_data)
+  .then(res => {
+    // agent_chats_data returned from request_agent_chats
+    return Promise.resolve(res);
+  }) 
+}
+
+let chat_model = ["id", "tickets", "visitor_name", "visitor_id", "agents", "rate", "duration", "chat_start_url", "referrer", "group", "started", "tags", "pre_chat_survey", "engagement", "ended", "prechat_survey"];
--- app/index.js
+++ app/index.js
@@ -1,77 +1,39 @@
 require('dotenv').config();
-const rp = require("request-promise");
-const _ = require('lodash');
 const agents = require('./agents');
+const chats = require('./chats');
 
-agents.request_livechat_agents()
-.then((res) => {
-  // FOR EACH AGENT, CALL REQUEST_AGENT_CHATS(agent) 
-  res.forEach((agent) => {
-    // console.log("AGENT: ", agent);
-    build_agent_chats(agent);
-  })
-
-})
-.catch((err) => {
-  console.log("Error fetching agents: ", err);
-})
-
-// Then, pull report for each agent
-// Docs - https://docs.livechatinc.com/rest-api/#get-list-of-chats
-// Example - https://api.livechatinc.com/v2/chats?date_from=2018-01-22&date_to=2018-01-22&timezone=America/Los_Angeles&page=...
-let allAgentChats = {};
-let chat_model = ["id", "tickets", "visitor_name", "visitor_id", "agents", "rate", "duration", "chat_start_url", "referrer", "group", "started", "tags", "pre_chat_survey", "engagement", "ended", "prechat_survey"];
-
-function build_agent_chats(agent) {
-  allAgentChats[agent] = [];
-  let chat_params = {
-    date_from: "2018-02-01",
-    date_to: "2018-02-28",
-    timezone: "America/Los_Angeles",
-    agent: agent
+/*
+  Iterate through arr of agents and build allAgentChats:
+  {
+    agent: [chats],
+    agent: [chats],
+    etc
   }
-  let chat_options = {
-    uri: "https://api.livechatinc.com/v2/chats",
-    json: true,
-    qs: chat_params
-  }
-  request_agent_chats(chat_options);
-}
+*/
+var testAgents = ['bhaas@classy.org', 'stelfer@classy.org'];
 
-function request_agent_chats(chat_options) {
-  chat_options.qs.page = chat_options.qs.page || 1;
-  // agent_chats should be set to a blank [] in build_agent_chats()
-  let agent_chats = allAgentChats[chat_options.qs.agent];
-  rp(chat_options)
-  .auth(process.env.EMAIL, process.env.API_KEY)
-  .then((res) => {
-    // push chats to with data/columns that we need
-    res.chats.forEach((chat) => {
-      let filtered_chat = _.pick(chat, chat_model);
-      agent_chats.push(filtered_chat);
+testAgents.forEach(agent => {
+  chats.agent_chats(agent)
+    // response should be all of that agent's chats
+    .then(res => {
+      console.log("\n\n\nAgent: ", agent);
+      console.log("Chats? ", res);
     })
-    // collect responses from page 2 onward (livechat provides 25 chats per page)
-    if (chat_options.qs.page < res.pages) {
-      chat_options.qs = _.merge(chat_options.qs, {"page":(chat_options.qs.page + 1)});
-      setTimeout(next_request, 1500);
-      function next_request() {
-        return request_agent_chats(chat_options);
-      }
-    } else {
-      console.log("\n\n\n\n\n\nAgent: ", chat_options.qs.agent);
-      console.log("Total agent chats: ", agent_chats.length);
-      // print single agent chats to convert to CSV
-      console.log(JSON.stringify(agent_chats));
-      // Store that agent's chats in allAgentChats object
-      // allAgentChats[agent] = agent_chats;
-      // test if allAgentChats filled with agent's chats
-      return agent_chats;
-    }
-  })
-  .catch((err) => {
-    console.log("Error: ", err)
-  })
-};
+    .catch(err => {
+      console.log("Error fetching agent: ", err);
+    })
+})
+
+// let allAgentChats = {};
+// agents.request_livechat_agents()
+// .then((res) => {
+//   // FOR EACH AGENT, CALL REQUEST_AGENT_CHATS(agent) 
+//   res.forEach((agent) => {
+//     // console.log("AGENT: ", agent);
+//     agent_chats(agent);
+//   })
+
+// })
 
 
 
